b58013365554e5418a2612fa016fc738686c3d83
Eunyoung KANG <greenkey20@gmail.com>
2023-05-31 18:21:07 +0900

    test: test query methods [item]

--- src/main/java/com/greenkey/book/domain/item/ItemRepository.java
+++ src/main/java/com/greenkey/book/domain/item/ItemRepository.java
@@ -10,5 +10,12 @@ public interface ItemRepository extends JpaRepository<Item, Long> {
     List<Item> findByName(String name);
 
     // 2023.5.31(수) 1h10
-    List<Item> findByNameOrDescription(String name, String description);
+    List<Item> findByNameOrDescription(String name, String description); // where i1_0.name=? or i1_0.description=?
+
+    // 2023.5.31(수) 18h
+    List<Item> findByPriceLessThanEqual(int price); // where i1_0.price<=?
+
+    // 2023.5.31(수) 18h10
+    // Java 객체(o) db 컬럼(x) 기준 속성명 기재 -> No property 'itemId' found for type 'Item'
+    List<Item> findByPriceGreaterThanOrderByIdDesc(int price); // where i1_0.price>? order by i1_0.item_id desc
 }
--- src/test/java/com/greenkey/book/domain/item/ItemRepositoryTest.java
+++ src/test/java/com/greenkey/book/domain/item/ItemRepositoryTest.java
@@ -17,6 +17,10 @@ import static org.assertj.core.api.Assertions.assertThat;
 @TestPropertySource(locations = "classpath:application-test.yml") // 테스트 코드 실행 시에는 h2 db 사용
 class ItemRepositoryTest {
     private String name = "테스트 상품";
+    private String description = "테스트 상품 상세 설명";
+    private int price = 10000;
+    private int stockNum = 100;
+    private ItemSellStatus itemSellStatus = ItemSellStatus.SELL;
 
     @Autowired // 사용하기 위해 Bean 주입
     ItemRepository itemRepository;
@@ -30,18 +34,12 @@ class ItemRepositoryTest {
     @DisplayName("상품 저장 테스트")
     public void createItemTest() {
         // given
-        String name = "테스트 상품";
-        int price = 10000;
-        int stockNum = 100;
-        String description = "테스트 상품 상세 설명";
-        ItemSellStatus itemSellStatus = ItemSellStatus.SELL;
-
         itemRepository.save(Item.builder()
-                .name(name)
-                .price(price)
-                .stockNum(stockNum)
-                .description(description)
-                .itemSellStatus(itemSellStatus)
+                .name(this.name)
+                .price(this.price)
+                .stockNum(this.stockNum)
+                .description(this.description)
+                .itemSellStatus(this.itemSellStatus)
                 .build());
 
         // when
@@ -55,18 +53,16 @@ class ItemRepositoryTest {
 
     // 2023.5.31(수) 1h15 테스트용 상품 10개 만드는 메서드
     public void createItemsList() {
-        int price = 10000;
         int stockNum = 100;
-        String description = "테스트 상품 상세 설명";
         ItemSellStatus itemSellStatus = ItemSellStatus.SELL;
 
         for (int i = 1; i <= 10; i++) {
             itemRepository.save(Item.builder()
                     .name(this.name + i)
-                    .price(price)
-                    .stockNum(stockNum)
-                    .description(description + i)
-                    .itemSellStatus(itemSellStatus)
+                    .price(this.price + i)
+                    .stockNum(this.stockNum)
+                    .description(this.description + i)
+                    .itemSellStatus(this.itemSellStatus)
                     .build()); // Item(id=1, name=테스트 상품, price=10000, stockNum=100, description=테스트 상품 상세 설명, itemSellStatus=SELL)
 
             // itemRepository에 테스트 상품 하나만 저장되는 건 이 메서드의 내용 때문이 아니었음 -> 실수로 이 메서드를 호출하지 않고 createItemTest()를 호출했기 때문
@@ -108,5 +104,50 @@ class ItemRepositoryTest {
         Item item = itemsList.get(0);
         assertThat(itemsList.size()).isEqualTo(1);
         assertThat(item.getName()).isEqualTo(this.name + 1);
+    }
+
+    // 2023.5.31(수) 17h50
+    @Test
+    @DisplayName("상품명 또는 상품 상세 설명 검색 테스트")
+    public void findByNameOrDescriptionTest() {
+        // given
+        createItemsList();
+
+        // when
+        List<Item> searchResult1 = itemRepository.findByNameOrDescription(this.name + 1, this.description + 4); // case1)
+        List<Item> searchResult2 = itemRepository.findByNameOrDescription(null, this.description + 4); // case2)
+
+        // then
+        assertThat(searchResult1.size()).isEqualTo(2); // case1)
+        assertThat(searchResult2.size()).isEqualTo(1); // case2)
+    }
+
+    // 2023.5.31(수) 18h
+    @Test
+    @DisplayName("특정 가격 이하 상품 검색 테스트")
+    public void findByPriceLessThanEqualTest() {
+        // given
+        createItemsList();
+
+        // when
+        List<Item> searchResult = itemRepository.findByPriceLessThanEqual(this.price + 4);
+
+        // then
+        assertThat(searchResult.size()).isEqualTo(4);
+    }
+
+    // 2023.5.31(수) 18h10
+    @Test
+    @DisplayName("특정 가격 초과 상품 검색 + 상품id 내림차순 정렬 테스트")
+    public void findByPriceGreaterThanOrderByIdDescTest() {
+        // given
+        createItemsList(); // @AfterEach로 각 단위 테스트 후 db 저장 내용 지워도 pk는 계속 쌓인다?
+
+        // when
+        List<Item> searchResult = itemRepository.findByPriceGreaterThanOrderByIdDesc(this.price + 4);
+
+        // then
+        assertThat(searchResult.size()).isEqualTo(6);
+        assertThat(searchResult.get(0).getId()).isEqualTo(10);
     }
 }
